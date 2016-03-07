bce97df8cd3450c7d50de37e34b4489ef4460dfa
Supinfo <supinfo@supinfos-mac.local>
2016-03-07 22:19:14 +0100

    ajout d'une seconde Vue et création de la classe controller correspondante

--- ProjetTransversaux/ViewController.swift
+++ ProjetTransversaux/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBOutlet weak var UsernameTxt: UITextField!
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
---
+++ ProjetTransversaux/ViewControllerTwo.swift
@@ -0,0 +1,15 @@
+//
+//  ViewControllerTwo.swift
+//  ProjetTransversaux
+//
+//  Created by Supinfo on 07/03/16.
+//  Copyright © 2016 Ingesup. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewControllerTwo:UIViewController
+{
+    
+}
