ba89966a71521415971549b0f0b2c4c0b45f6996
Supinfo <supinfo@supinfos-mac.local>
2016-03-08 11:07:39 +0100

    ajout du deuxième View controller et création des outlets

---
+++ ProjetTransversaux/TwoViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TwoViewController.swift
+//  ProjetTransversaux
+//
+//  Created by Supinfo on 08/03/16.
+//  Copyright © 2016 Ingesup. All rights reserved.
+//
+
+import UIKit
+
+class TwoViewController: UIViewController {
+
+    var message = String()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            lblMessage.text = message
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var lblMessage: UILabel!
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
