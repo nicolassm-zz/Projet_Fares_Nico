a771cb99ed50d750f1c12489ca8e44e8c4c5658f
Nicolas <nshihmen@gmail.com>
2016-03-08 16:47:04 +0100

    Debut de login page

---
+++ ProjetTransversal/ProjetTransversal/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,27 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetTransversal.ViewModels
+{
+    class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            //SimpleIoc.Default.Register<ICardApi, CardApi>();
+            //SimpleIoc.Default.Register<IImageLoader, ImageLoader>();
+
+            SimpleIoc.Default.Register<MainPageViewModel>();
+            SimpleIoc.Default.Register<LoginPageViewModel>();
+        }
+
+        public MainPageViewModel MainPage => ServiceLocator.Current.GetInstance<MainPageViewModel>();
+        public LoginPageViewModel SettingsPage => ServiceLocator.Current.GetInstance<LoginPageViewModel>();
+    }
+}
